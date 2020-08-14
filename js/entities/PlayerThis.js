a90cbeceabff307e9a0a641850533827553665d2
Richard Wells <rwells1703@gmail.com>
2020-08-14 12:53:38 +0100

    Improved grenade throwing physics.

--- js/entities/PlayerThis.js
+++ js/entities/PlayerThis.js
@@ -8,12 +8,13 @@ export class PlayerThis extends Player {
         // Increments every time a grenade is thrown
         this.grenadeId = 0;
     
-        this.GRENADE_THROW_SPEED = 1000;
+        //this.GRENADE_THROW_SPEED = 1000;
+        this.GRENADE_THROW_SPEED = 3;
         this.GRENADE_THROW_TIMEOUT = 400;
-        this.GRENADE_THROW_RANGE = 300;
+        this.GRENADE_THROW_RANGE = 400;
 
         this.throwGrenadeDelta = 0;
-
+        
         // Throw a grenade
         scene.input.on('pointerdown', pointer => {
             // Disables grenade throws within joystick (if joystick is enabled)
@@ -33,19 +34,43 @@ export class PlayerThis extends Player {
     throwGrenade(xTarget, yTarget, grenadeType) {
         let xDistance = xTarget - this.x;
         let yDistance = yTarget - this.y;
-        let distance = Math.sqrt(xDistance**2 + yDistance**2);
+
+        if (xDistance > this.GRENADE_THROW_RANGE) {
+            xDistance = this.GRENADE_THROW_RANGE;
+        }
+
+        if (xDistance < -this.GRENADE_THROW_RANGE) {
+            xDistance = -this.GRENADE_THROW_RANGE;
+        }
+
+        
+        if (yDistance > this.GRENADE_THROW_RANGE) {
+            yDistance = this.GRENADE_THROW_RANGE;
+        }
+
+        if (yDistance < -this.GRENADE_THROW_RANGE) {
+            yDistance = -this.GRENADE_THROW_RANGE;
+        }
+
+        console.log(xDistance, yDistance);
+        /*let distance = Math.sqrt(xDistance**2 + yDistance**2);
 
         let speedCoefficient = distance / this.GRENADE_THROW_RANGE;
 
         // If they are throwing outside the range
         if (speedCoefficient > 1) {
             speedCoefficient = 1;
+        }*/
+
+        let xVel = xDistance * this.GRENADE_THROW_SPEED;
+        let yVel = yDistance * this.GRENADE_THROW_SPEED;
+        //let xVel = xDistance/distance * this.GRENADE_THROW_SPEED * speedCoefficient + this.body.velocity.x;
+        //let yVel = yDistance/distance * this.GRENADE_THROW_SPEED * speedCoefficient + this.body.velocity.y;
+
+        let grenade;
+        if (grenadeType == 'ConcussionGrenade') {
+            grenade = new GrenadeConcussion(this.scene, this.x, this.y, this.grenadeId, this, xVel, yVel);
         }
-
-        let xVel = xDistance/distance * this.GRENADE_THROW_SPEED * speedCoefficient + this.body.velocity.x;
-        let yVel = yDistance/distance * this.GRENADE_THROW_SPEED * speedCoefficient + this.body.velocity.y;
-
-        let grenade = new GrenadeConcussion(this.scene, this.x, this.y, this.grenadeId, this, xVel, yVel);
 
         let newGrenade = {
             grenadeId: grenade.id,
